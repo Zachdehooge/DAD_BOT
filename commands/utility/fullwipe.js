ce16a8720bb8ce319ad6fc15463242fa1d08875e
Zach <47896974+Zachdehooge@users.noreply.github.com>
2025-02-21 11:13:49 -0500

    feat(commands): Add wipe time option to fullwipe and mapwipe commands

--- commands/utility/fullwipe.js
+++ commands/utility/fullwipe.js
@@ -2,35 +2,49 @@ const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('fullwipe')
-        .setDescription('Full Wipe Announcement')
-        .addStringOption(option =>
-            option
-                .setName('map')
-                .setDescription('Map URL'))
-        .addStringOption(option =>
-            option
-                .setName('map_thumbnail')
-                .setDescription('Map Thumbnail PNG')),
+  data: new SlashCommandBuilder()
+    .setName('fullwipe')
+    .setDescription('Full Wipe Announcement')
+    .addStringOption((option) =>
+      option
+        .setName('wipe_time')
+        .setDescription('Wipe Time in UNIX Timestamp')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName('map').setDescription('Map URL').setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('map_thumbnail')
+        .setDescription('Map Thumbnail PNG')
+        .setRequired(true)
+    ),
 
-    async execute(interaction) {
-        //await interaction.deferReply();
+  async execute(interaction) {
+    //await interaction.deferReply();
 
-        const truly = interaction.options.getString('map')
-        const thumb = interaction.options.getString('map_thumbnail')
+    const truly = interaction.options.getString('map');
+    const thumb = interaction.options.getString('map_thumbnail');
+    const wipe_time = interaction.options.getString('wipe_time');
+    const timestampString = `<t:${wipe_time}:F>`;
 
-        const exampleEmbed = new EmbedBuilder()
-            .setColor(0xef2929)
-            .setTitle('Rust Full Wipe')
-            .setAuthor({ name: 'Dads After Dark', iconURL: 'https://i.imgur.com/dlecsVP.png', url: 'https://i.imgur.com/dlecsVP.png' })
-            .setDescription('Map Wipe Announcement')
-            .addFields(
-                { name: 'Map Announcement', value: `${truly}` },
-                //.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-            )
-            .setImage(`${thumb}`)
-            .setTimestamp()
-        await interaction.reply({ embeds: [exampleEmbed] })
-    },
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0xef2929)
+      .setTitle('Rust Full Wipe')
+      .setAuthor({
+        name: 'Dads After Dark',
+        iconURL: 'https://i.imgur.com/dlecsVP.png',
+        url: 'https://i.imgur.com/dlecsVP.png',
+      })
+      .setDescription('Full Wipe Announcement')
+      .addFields(
+        { name: 'Map Announcement', value: `${truly}` },
+        { name: 'Wipe Time', value: `${timestampString}` }
+        //.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+      )
+      .setImage(`${thumb}`);
+    //.setTimestamp();
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
 };
